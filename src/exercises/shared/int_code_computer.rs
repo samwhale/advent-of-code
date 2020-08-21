0016d58b7056485218eb36fd05052885e36651f6
samwhale <samuel.estrella@formidable.com>
2020-08-20 20:01:46 -0600

    Add tests, put message on stack instead of heap

--- src/exercises/shared/int_code_computer.rs
+++ src/exercises/shared/int_code_computer.rs
@@ -23,14 +23,14 @@ impl IntCodeComputer {
     Self { code: Vec::new() }
   }
 
-  fn read(&mut self, message: String) {
+  fn read(&mut self, message: &str) {
     for val in message.trim().split(",") {
       let val = val.parse::<u32>().unwrap();
       self.code.push(val);
     }
   }
 
-  pub fn process_code(mut self, message: String) -> Vec<u32> {
+  pub fn process_code(mut self, message: &str) -> Vec<u32> {
     self.read(message);
 
     let code_len = self.code.len();
@@ -65,5 +65,28 @@ impl IntCodeComputer {
     }
 
     self.code
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn process_code_test() {
+    let comp1 = IntCodeComputer::new();
+    let comp2 = IntCodeComputer::new();
+    let comp3 = IntCodeComputer::new();
+    let comp4 = IntCodeComputer::new();
+    assert_eq!(comp1.process_code("1,0,0,0,99"), vec![2, 0, 0, 0, 99]);
+    assert_eq!(comp2.process_code("2,3,0,3,99"), vec![2, 3, 0, 6, 99]);
+    assert_eq!(
+      comp3.process_code("2,4,4,5,99,0"),
+      vec![2, 4, 4, 5, 99, 9801]
+    );
+    assert_eq!(
+      comp4.process_code("1,1,1,4,99,5,6,0,99"),
+      vec![30, 1, 1, 4, 2, 5, 6, 0, 99]
+    );
   }
 }
