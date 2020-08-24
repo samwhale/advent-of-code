1196b2350c613f0784cfac2d0b0d9b3f3758946c
samwhale <samuel.estrella@formidable.com>
2020-08-23 21:36:46 -0600

    Finish part 2 of day 6

--- src/exercises/days/day6.rs
+++ src/exercises/days/day6.rs
@@ -1,20 +1,24 @@
 use super::super::super::utils;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub struct OrbitCalculator {
   orbits: HashMap<String, String>,
 }
 
-impl<'b> OrbitCalculator {
+impl OrbitCalculator {
   pub fn new() -> Self {
     Self {
       orbits: HashMap::new(),
     }
   }
 
-  pub fn run(&mut self, message: Vec<String>) -> u32 {
+  pub fn run(&mut self, message: Vec<String>) -> (u32, u32) {
     self.parse_data(message);
-    self.count_orbits()
+    (
+      self.count_orbits(),
+      self.get_distance_between_nodes(&String::from("YOU"), &String::from("SAN")),
+    )
   }
 
   fn parse_data(&mut self, message: Vec<String>) {
@@ -41,9 +45,32 @@ impl<'b> OrbitCalculator {
     }
     num_orbits
   }
+
+  fn get_path_to_center(&self, node: &String) -> HashSet<&String> {
+    let mut path: HashSet<&String> = HashSet::new();
+    let mut maybe_center = self.orbits.get(node);
+    loop {
+      match maybe_center {
+        Some(center) => {
+          path.insert(center);
+          maybe_center = self.orbits.get(center);
+        }
+        None => break,
+      }
+    }
+
+    path
+  }
+
+  fn get_distance_between_nodes(&self, n1: &String, n2: &String) -> u32 {
+    let path1: HashSet<&String> = self.get_path_to_center(n1);
+    let path2: HashSet<&String> = self.get_path_to_center(n2);
+    let path_to_san: HashSet<_> = path1.symmetric_difference(&path2).collect();
+    path_to_san.len() as u32
+  }
 }
 
-pub fn get_num_orbits(message: Vec<String>) -> u32 {
+pub fn get_orbital_data(message: Vec<String>) -> (u32, u32) {
   let mut orbit_calculator = OrbitCalculator::new();
   orbit_calculator.run(message)
 }
@@ -51,7 +78,12 @@ pub fn get_num_orbits(message: Vec<String>) -> u32 {
 pub fn main() {
   println!("--- Day 6 ---");
   let orbits = utils::read_file_into_vector("./src/exercises/data/data-day6.txt");
-  println!("Num direct + indirect orbits: {}", get_num_orbits(orbits));
+  let (total_orbits, distance_to_santa) = get_orbital_data(orbits);
+  println!("Num direct + indirect orbits: {}", total_orbits);
+  println!(
+    "Distance to planet santa is orbiting: {}",
+    distance_to_santa
+  );
 }
 
 #[cfg(test)]
@@ -72,10 +104,11 @@ mod tests {
     orbits.push(String::from("J)K"));
     orbits.push(String::from("K)L"));
 
-    assert_eq!(get_num_orbits(orbits), 42);
+    assert_eq!(get_orbital_data(orbits).0, 42);
   }
 
-  fn transfers_needed_test() {
+  #[test]
+  fn transfers_needed_get_orbital_data() {
     let mut orbits: Vec<String> = Vec::new();
     orbits.push(String::from("COM)B"));
     orbits.push(String::from("B)C"));
@@ -91,6 +124,6 @@ mod tests {
     orbits.push(String::from("K)YOU"));
     orbits.push(String::from("I)SAN"));
 
-    // assert_eq!(get_min_transfers(orbits), 4);
+    assert_eq!(get_orbital_data(orbits).1, 4);
   }
 }
