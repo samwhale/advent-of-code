9fc1002170f32bc5f6609f87b0b54ae7d6134566
samwhale <samuel.estrella@formidable.com>
2020-08-26 20:11:08 -0600

    Make it slightly more pretty

--- src/exercises/days/day9.rs
+++ src/exercises/days/day9.rs
@@ -1,16 +1,19 @@
 use super::super::super::utils;
 use super::super::shared::int_code_computer::IntCodeComputer;
 
+pub fn sensor_boost(message: &str, boost_mode: bool) -> i64 {
+  let mut int_code_computer = IntCodeComputer::new(&message);
+  let mode = if boost_mode { 2 } else { 1 };
+  int_code_computer.add_inputs(vec![mode]);
+  int_code_computer.process_code().output[0]
+}
+
 pub fn main() {
   println!("--- Day 9 ---");
   let message = utils::read_file_into_string("src/exercises/data/data-day9.txt");
-  let mut int_code_computer1 = IntCodeComputer::new(&message);
-  int_code_computer1.add_inputs(vec![1]);
-  let result1 = int_code_computer1.process_code();
-  println!("Test mode -> output: {:?}", result1.output[0]);
+  let result1 = sensor_boost(&message, false);
+  let result2 = sensor_boost(&message, true);
+  println!("Test mode -> output: {:?}", result1);
   println!("--- Part 2 ---");
-  let mut int_code_computer2 = IntCodeComputer::new(&message);
-  int_code_computer2.add_inputs(vec![2]);
-  let result2 = int_code_computer2.process_code();
-  println!("BOOST MODE -> output: {:?}", result2.output[0]);
+  println!("BOOST MODE -> output: {:?}", result2);
 }
